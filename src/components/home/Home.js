a485585c13eb3ab93182c58331e403b2c15d5a36
ariannysOronoz <ariom0910@gmail.com>
2023-05-09 08:53:27 -0300

    created css home section

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,69 +5,54 @@ export default function Home() {
 
     return (
         <div className="home">
-
-            <h1>Home</h1>
+          <h1>Home</h1>
             <div className="player">
-            <div className="player-online">
-                <div className="online -title">
-                <h1>Players Online </h1>
+                <div className="online">
+                    <div className="online-title"><h1>Players Online </h1></div>
+                    <div className="player-btn -01">
+                        <button className="online player-btns">000</button>
+                    </div>
                 </div>
-                <div className="player-btn -01">
-                    <button className="online player-btns">000</button>
+                <div className="total">
+                    <div className="total-title"><h1>Total Players</h1></div>
+                    <div className="player-btn -02">
+                        <button className="total player-btns">000</button>
+                    </div>
                 </div>
+                <div className="account">
+                    <div className="account-title"><h1>Total Accounts </h1></div>
+                    <div className="player-btn -03">
+                        <button className="accounts player-btns">000</button>
+                    </div>
+                </div>   
             </div>
-
-            <div className="player-total">
-                <div className="total -title">
-                <h1>Total Players</h1>
-                </div>
-                <div className="player-btn -02">
-                    <button className="total player-btns">000</button>
-                </div>
-            </div>
-
-            <div className="totalAccounts">
-                <div className="account -title">
-                <h1>Total Accounts </h1>
-                </div>
-                <div className="player-btn -03">
-                    <button className="accounts player-btns">000</button>
-                </div>
-            </div>
-            </div>
-
             <div className="gameAndLogin">
-                <div className="titleLogin">
-                    <h1>Game and login Server</h1> 
-                </div>
+                <div className="sectiongame">
+                <div className="titleLogin"><h1>Game and login Server</h1></div>
                 <div className="btns">
-                    <button className="bnt4 -btn">Disconnect server</button>
-                    <button className="bnt5 -btn">Reboot server</button>
+                    <button className="bnt4 player-btns">Disconnect server</button>
+                    <button className="bnt5 player-btns">Reboot server</button>
+                </div>
                 </div>
             </div>
-
             <div className="server">
-
-                <div className="login">
-                    <div className="titleServer">
-                        <h1>Login Server</h1>
-                    </div>
-                    <div className="btnServer">
-                        <button className="bnt6 -btn">Disconnect server</button>
-                        <button className="bnt7 -btn">Reboot server</button>
-                        <button className="bnt8 -btn">Disconnect server</button>
-                    </div>
-                </div>
-
-                <div className="game">
-                        <div className="titleGame">
-                            <h1>Game Server</h1>
+                <div className="listserver">
+                    <div className="sectionserver">
+                        <div className="titleServer"><h1>login Server</h1></div>
+                        <div className="btnServer">
+                            <button className="bnt6 player-btns">Disconnect server</button>
+                            <button className="bnt7 player-btns">Reboot server</button>
+                            <button className="bnt8 player-btns">Reboot server</button>
                         </div>
+                    </div>
+                    <div className="sectiongame">
+                        <div className="titleGame"><h1>Game Server</h1></div>
                         <div className="btnGame">
-                            <button className="bnt9 -btn">Disconnect server</button>
-                            <button className="bnt10 -btn">Reboot server</button>
-                            <button className="bnt11 -btn">Disconnect server</button>
+                            <button className="bnt9 player-btns">Disconnect server</button>
+                            <button className="bnt10 player-btns">Reboot server</button>
+                            <button className="bnt11 player-btns">Disconnect server</button>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
