0e72b174faaf87d17f09fc81bfde656724a3afb4
ariannysOronoz <ariom0910@gmail.com>
2023-05-10 18:27:57 -0300

    created css chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -5,26 +5,27 @@ export default function Home() {
 
     return (
 
-        <div className="allChat">
-            <div className="titleChat"><h1>Chat</h1></div>
-            <div className="detailChat">
-              <div className="detailColumn1">
-                <div className="column1 -box">
-                    <div className="chat -box"></div>
-                    <div className="msg -box">
-                        <div className="msg -box-chat">Your message</div>
-                        <div className="send -box-chat">
-                            <button className="send -btn-chat">send</button>
-                        </div>
+        <div className="Chat">
+          <div className="title">
+            <h1 className="titleChat">Chat</h1>
+            <h1 className="titleplayers">Players Online</h1>
+            </div>
+              <div className="Chating">
+                <div className="columnChat">
+                  <div className="boxChat"></div>
+                  <div className="boxMsg">
+                    <div className="message">
+                        <div className="msg-btnchat">your message</div>
                     </div>
-                </div>
-                <div className="column2 -box">
-                    <div className="users">
-                        <div className="players -online"></div>
+                    <div className="send">
+                        <button className="send-btnchat">send</button>
                     </div>
+                  </div>
                 </div>
-              </div>
-            </div> 
+                <div className="columnPlayer">
+                  <div className="players"></div>
+                </div>
+             </div> 
         </div>    
     );
 };
